5b98fad8691123ad19524054d6865614241d0a29
abdullah <abdullah.balikci20@ogr.atauni.edu.tr>
2025-04-19 13:18:05 +0300

    Initial Create

---
+++ Example.Logstash.ConsoleApp/Example.Logstash.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using Serilog.Sinks.Network;
+
+var projectName = "test";
+var id = 3;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.TCPSink("tcp://localhost:5000")
+    // .WriteTo.UDPSink("udp://localhost",5001)
+    .Enrich.WithProperty("project_name",projectName)
+    .CreateLogger();
+
+Log.Information("Uygulama Başladı");
+
+Log.Information("{id}'li Kullanıcı Bir Post Paylaştı", id);
+
+
+Console.ReadLine();
+
+Log.Fatal("Uygulama da bir hata oluştu");
+
+Console.ReadLine();
